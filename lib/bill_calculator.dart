0a660b835ca38e6f1010c875e89a7801f220f42d
igorurbaniak <igorurbaniakq@wp.pl>
2022-09-03 09:50:21 +0200

    Project setup

---
+++ lib/bill_calculator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BillCalculator extends StatefulWidget {
+  const BillCalculator({Key? key}) : super(key: key);
+
+  @override
+  State<BillCalculator> createState() => _BillCalculatorState();
+}
+
+class _BillCalculatorState extends State<BillCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
