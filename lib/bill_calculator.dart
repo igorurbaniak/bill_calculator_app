8474a3f5d0f9b0ab44cd6b75b85db068baba02ee
igorurbaniak <igorurbaniakq@wp.pl>
2022-09-03 10:38:52 +0200

    Add upper container of home screen

--- lib/bill_calculator.dart
+++ lib/bill_calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BillCalculator extends StatefulWidget {
   const BillCalculator({Key? key}) : super(key: key);
@@ -8,8 +9,106 @@ class BillCalculator extends StatefulWidget {
 }
 
 class _BillCalculatorState extends State<BillCalculator> {
+  TextStyle createdstyle = GoogleFonts.fredokaOne(
+    fontSize: 18,
+    color: Colors.white,
+    fontWeight: FontWeight.w500,
+  );
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+        ),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 40),
+              child: Text(
+                'Bill Calculator',
+                style: GoogleFonts.fredokaOne(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                      'images/black_graphite.jpg',
+                    ),
+                    fit: BoxFit.fill),
+                // color: Colors.yellow,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(12),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Total',
+                          style: GoogleFonts.fredokaOne(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          '100',
+                          style: GoogleFonts.fredokaOne(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Friends', style: createdstyle),
+                            Text('Tax', style: createdstyle),
+                            Text('Tip', style: createdstyle),
+                          ],
+                        ),
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('6', style: createdstyle),
+                            Text('23%', style: createdstyle),
+                            Text('10', style: createdstyle),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
